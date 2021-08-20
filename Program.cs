f7442be0374750ad72d35953a753b1a6598893a5
Nathan Van Zyl <6406@ctucareer.co.za>
2021-08-20 15:47:58 +0200

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace PGR521_FA2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("What is your Name?");
+            string Name = Console.ReadLine();
+            Console.WriteLine("What project are you working on?");
+            string Task = Console.ReadLine();
+            Console.WriteLine("What is your icas?");
+            string Icas = Console.ReadLine();
+            Console.WriteLine("-------------------------------------------------------");
+            Console.WriteLine(Name + " " + Icas + " " + "is working on" + " " + Task);
+        }
+    }
+}
